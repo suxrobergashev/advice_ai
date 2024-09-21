1e5842621dfd0285930b33d10efb64371e3de323
suxrob <suxrobergashev2102@fgamail.com>
2024-09-21 22:00:37 +0000

    change admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Users, Answer, Questions, Chat, Summary
 
-# Register your models here.
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'age', 'gender', 'phone_number')
+    search_fields = ('full_name', 'age', 'gender', 'phone_number')
+    list_filter = ('age', 'gender', 'phone_number')
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'answer')
+
+@admin.register(Questions)
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question')
